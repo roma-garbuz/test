271beab00e35421ddd953a909d59cba884250a32
roma-garbuz <roma-garbuz@ukr.net>
2020-04-17 18:38:47 +0300

    Updates

---
+++ precache-manifest.b129dc7d00bac99d44fbf7107fbfe78c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f36b7ff643d6d9de0ab82128511cf5ef",
+    "url": "/test/index.html"
+  },
+  {
+    "revision": "d0996aa954505e55710c",
+    "url": "/test/static/css/main.2045f49b.chunk.css"
+  },
+  {
+    "revision": "b79921ff034cbeff985c",
+    "url": "/test/static/js/2.56a368a5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/test/static/js/2.56a368a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d0996aa954505e55710c",
+    "url": "/test/static/js/main.88ce902f.chunk.js"
+  },
+  {
+    "revision": "fa7ff639614e7bf64331",
+    "url": "/test/static/js/runtime-main.2aabfe4a.js"
+  }
+]);
